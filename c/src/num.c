a3e65bc17152957fd227a8069759fcbfdd836af7
Nate Brown <nravinbrown@gmail.com>
2025-03-02 17:34:38 -0600

    some arithmetic

--- c/src/num.c
+++ c/src/num.c
@@ -9,8 +9,26 @@
 #include "complex.c"
 #include <stdint.h>
 
-#define n_new(x) _Generic((X), int64_t: n_z, double: n_r, complex: n_c)(X)
-#define DBG(NUM) printf("tag: %d, z: %lld, r: %f, c: %f + i%f\n", NUM.tag, NUM.val.z, NUM.val.r, NUM.val.c.r, NUM.val.c.i)
+#define n_new(X) _Generic((X), int64_t: n_z, double: n_r, complex: n_c)(X)
+#define DBG(NUM)                                                               \
+  printf("tag: %d, z: %ld, r: %f, c: %f + i%f\n", NUM.tag, NUM.val.z,          \
+         NUM.val.r, NUM.val.c.r, NUM.val.c.i)
+
+#define NMATCH(X, Zv, Rv, Cv)                                                  \
+  int64_t z;                                                                   \
+  double r;                                                                    \
+  complex c;                                                                   \
+  switch (X.tag) {                                                             \
+  case Z:                                                                      \
+    z = X.val.z;                                                               \
+    return Zv;                                                                 \
+  case R:                                                                      \
+    r = X.val.r;                                                               \
+    return Rv;                                                                 \
+  case C:                                                                      \
+    c = X.val.c;                                                               \
+    return Cv;                                                                 \
+  }
 
 typedef union intern {
   int64_t z;
@@ -22,8 +40,6 @@ typedef struct num {
   intern val;
   tag tag;
 } num;
-
-const num NBLANK = {.tag = Z, .val = {.z = 0}};
 
 const num n_z(int64_t x) {
   num ret;
@@ -46,49 +62,61 @@ const num n_c(complex x) {
   return ret;
 }
 
-void n_zero(num *x) {
-  switch (x->tag) {
-  case Z:
-    x->val.z = 0;
-    break;
-  case R:
-    x->val.r = 0;
-    break;
-  case C:
-    x->val.c = ZERO;
-    break;
-  }
-}
-
-void n_one(num*x) {
-    switch (x-> tag) {
-        case Z: x->val.z=1;
-        case R: x->val.r=1;
-        case C: x->val.c=ONE;
-    }
-}
+const double Re(num x) { NMATCH(x, (double)z, r, c.r) }
+const complex esc(num x) { NMATCH(x, c_new((double)z, 0.0), c_new(r, 0.0), c); }
 
 const num n_add(num a, num b) {
   num ret;
-  ret.tag = a.tag < b.tag ? a.tag : b.tag;
-  n_zero(&ret);
-  // TODO
+  ret.tag = a.tag > b.tag ? a.tag : b.tag;
+  switch (ret.tag) {
+  case Z:
+    ret.val.z = a.val.z + b.val.z;
+  case R:
+    ret.val.r = Re(a) + Re(b);
+  case C:
+    ret.val.c = c_add(esc(a), esc(b));
+  }
   return ret;
+}
+
+const num n_neg(num a) { NMATCH(a, n_z(-z), n_r(-r), n_c(c_neg(c))) }
+
+const num n_sub(num x, num y) { return n_add(x, n_neg(y)); }
+
+const num n_recip(num a) {
+  NMATCH(a, n_r(1 / (double)z), n_r(1 / r), n_c(c_recip(c)))
+}
+
+const num n_mul(num a, num b) {
+  num ret;
+  ret.tag = a.tag > b.tag ? a.tag : b.tag;
+  switch (ret.tag) {
+  case Z:
+    ret.val.z = a.val.z * b.val.z;
+  case R:
+    ret.val.r = Re(a) * Re(b);
+  case C:
+    ret.val.c = c_mul(esc(a), esc(b));
+  }
+  return ret;
+}
+
+const num n_div(num x, num y) { return n_mul(x, n_recip(y)); }
+
+const num n_exp(num x) {
+  NMATCH(x, n_r(exp((double)z)), n_r(exp(r)), n_c(c_exp(c)))
+}
+
+const num n_ln(num x) {
+  NMATCH(x, z > 0 ? n_r(log((double)z)) : n_c(c_new(log(-(double)z), M_PI)),
+         r > 0 ? n_r(log(r)) : n_c(c_new(log(-r), M_PI)), n_c(c_ln(c)))
 }
 
 #ifdef NUMMAIN
 
 #include <stdio.h>
 
-int main(void) {
-  for (tag tag = 0; tag <= 2; tag++) {
-    num num = NBLANK;
-    DBG(num);
-    num.tag = tag;
-    n_one(&num);
-    DBG(num);
-  }
-}
+int main(void) { printf("num.c"); }
 
 #endif
 #endif
