a0fe3ec1cfe7939ac314bfa2a415ea5bdef4a370
Nate Brown <nravinbrown@gmail.com>
2025-09-29 14:22:50 -0400

    added some more functions

--- c/src/main.c
+++ c/src/main.c
@@ -30,17 +30,18 @@ void command(char* line) {
 dif fcall(dif ans, char** line);
 
 dif calculate(dif ans, char** line) {
+    while (iswhitespace(**line) && **line != '\0') (*line)++;
     if (**line == '\0') return ans;
-    while (iswhitespace(**line)) (*line)++;
     dif op1, op2;
     switch (**line) {
         case '+': GET(1); return d_add(op1, op2);
+        case '-': GET(1); return d_sub(op1, op2);
         case '*': GET(1); return d_mul(op1, op2);
         case '/': GET(1); return d_div(op1, op2);
         case '^': GET(1); return d_pow(op1, op2);
-        case '-': GET1; (*line)++; return d_neg(op1);
+        case '_': (*line)++; GET1; return d_neg(op1);
         case '.': (*line)++; return ans;
-        case 'j': return d_const(n_c(c_new(0.0, 1.0)));
+        case 'j': (*line)++; return d_const(n_c(c_new(0.0, 1.0)));
     }
     double d;
     int n;
@@ -51,46 +52,57 @@ dif calculate(dif ans, char** line) {
     return fcall(ans, line);
 }
 
-#define FCALLCASE(s)                                                           \
+#define FCALLCASE(s) FCALLCASEG(s, d_##s(v))
+
+#define FCALLCASEG(s, ex)                                                      \
   if ((n = strpre(#s, *line)) != 0) {                                          \
     *line += n;                                                                \
-    return d_##s(calculate(ans, line));                                        \
-  } else
+    dif v = calculate(ans, line);                                              \
+    return ex;                                                                 \
+  }
 
-dif fcall(dif ans, char** line) {
-    int n;
-    FCALLCASE(sin)
-    FCALLCASE(cos)
-    FCALLCASE(exp)
-    FCALLCASE(ln)
-    return ans;
+dif fcall(dif ans, char **line) {
+  int n;
+  FCALLCASE(exp) FCALLCASE(ln);
+  FCALLCASE(sinh) FCALLCASE(cosh) FCALLCASE(tanh);
+  FCALLCASE(sin) FCALLCASE(cos) FCALLCASE(tan);
+  FCALLCASE(asinh) FCALLCASE(acosh) FCALLCASE(atanh);
+  FCALLCASE(asin) FCALLCASE(acos) FCALLCASE(atan);
+  FCALLCASEG(lb, d_div(d_ln(v), d_ln(d_const(n_z(2)))));
+  FCALLCASEG(lg, d_div(d_ln(v), d_ln(d_const(n_z(10)))));
+  return ans;
 }
 
 void writeln(dif d) {
-    num x = d.x;
-    switch (x.tag) {
-    case Z:
-        printf("%lli\n", x.val.z);
-        break;
-    case R:
-        printf("%lf\n", x.val.r);
-        break;
-    case C:
-        printf("%lf + %lfi\n", x.val.c.r, x.val.c.i);
-        break;
-    }
+  num x = d.x;
+  switch (x.tag) {
+  case Z:
+    printf("%lli\n", x.val.z);
+    break;
+  case R:
+    printf("%lf\n", x.val.r);
+    break;
+  case C:
+    printf("%lf + %lfi\n", x.val.c.r, x.val.c.i);
+    break;
+  }
 }
 
 int main(void) {
-    printf("Hello World!\n");
-    dif ans = d_new((int64_t)0);
-    while (true) {
-        char* line = readline("c> ");
-        if (line == NULL) return 0;
-        int len = strlen(line);
-        if (len == 0) continue;
-        while (iswhitespace(line[0])) line++;
-        if (line[0] == ':') command(line+1);
-        else writeln(ans = calculate(ans, &line));
-    }
+  printf("Hello World!\n");
+  dif ans = d_new((int64_t)0);
+  while (true) {
+    char *line = readline("c> ");
+    if (line == NULL)
+      return 0;
+    int len = strlen(line);
+    if (len == 0)
+      continue;
+    while (iswhitespace(line[0]))
+      line++;
+    if (line[0] == ':')
+      command(line + 1);
+    else
+      writeln(ans = calculate(ans, &line));
+  }
 }
